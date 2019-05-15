91885c93acd8adce7e8f0cffdab2ba1816620f7f
Alice Chen <alicejychen15@gmail.com>
2019-05-14 22:43:02 -0400

    add essentials

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  performance: {
+    hints: false,
+  }
+};
